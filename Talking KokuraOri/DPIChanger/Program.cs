00d16569b62e40c599d6de2f985ebe10dbd63a21
nmskm240 <bokaronogod@gmail.com>
2021-04-13 17:26:34 +0900

    出力データが300dpiで出力できるように

---
+++ Talking KokuraOri/DPIChanger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace DpiChanger
+{
+    class Program
+    {
+        static readonly string format = ".png";
+        static readonly string outputPaht = "../../../../output/";
+
+        static void Main(string[] args)
+        {
+            if(args.Length <= 0)
+            {
+                return;
+            }
+
+            var bmpOrg = new Bitmap(outputPaht + args[0] + format);
+            var bmpNew = new Bitmap(bmpOrg.Width, bmpOrg.Height);
+            bmpNew.SetResolution(300f, 300f);
+            var graphics = Graphics.FromImage(bmpNew);
+            graphics.DrawImage(bmpOrg, 0, 0, bmpOrg.Width, bmpOrg.Height);
+            graphics.Dispose();
+            bmpOrg.Dispose();
+            bmpNew.Save(outputPaht + args[0] + format);
+            bmpNew.Dispose();
+
+            return;
+        }
+    }
+}
--- 実行ファイル.bat
+++ 実行ファイル.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 cd /d %~dp0
 
+SET auth=
 SET filename=
 SET inputpath=../../input/
 SET formatWAV=.wav
@@ -15,8 +16,8 @@ ECHO  inputフォルダに存在するファイル名を入力してください
 ECHO +-------------------------------------------------------+
 SET /P filename=
  
-IF "%filename%"=="" GOTO :FILE_SELECT
-IF EXIST %filename%%formatWAV% (GOTO Wav2PNG) ELSE GOTO FILE_SELECT
+IF "%filename%"=="" (GOTO FILE_SELECT)
+IF EXIST %filename%%formatWAV% (GOTO Wav2PNG) ELSE (GOTO FILE_SELECT)
 
 :Wav2PNG
 cd /d %~dp0
@@ -25,11 +26,30 @@ start /wait Wav2PNG_20201025.exe filename=%inputpath%%filename%
 
 timeout 10
 
+ECHO +-------------------------------------------------------+
+ECHO  Bracode2KokuraOriを起動しますか.。[y/n]
+ECHO +-------------------------------------------------------+
+SET /P auth=
+IF %auth%==y (GOTO Barcode2KokuraOri) ELSE (GOTO END)
+
+:Barcode2KokuraOri
 cd /d %~dp0
 cd Talking KokuraOri/Debug
 call Barcode2KokuraOri.exe %filename%
+GOTO FIN
 
-cd %inputpath%
+:END
+cd /d %~dp0
+cd input
+move %filename%%formatPNG% ../output/
+cd /d %~dp0
+cd Talking KokuraOri/DPIChanger/bin/Debug
+call DPIChanger.exe %filename%
+GOTO FIN
+
+:FIN
+cd /d %~dp0
+cd input
 del %filename%%formatPNG% %filename%%formatCSV%
 
 PAUSE
